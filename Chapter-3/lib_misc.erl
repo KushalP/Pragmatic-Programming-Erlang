21efcda31dd3d44a70994785e5bc5a1d33393539
Kushal Pisavadia <kushi.p@gmail.com>
2010-07-03 00:13:31 +0100

    defining my own control abstractions using pattern matching and higher-order functions

--- Chapter-3/lib_misc.erl
+++ Chapter-3/lib_misc.erl
@@ -1,7 +1,19 @@
+-module(lib_misc).
+-export([
+	sum/1,
+	sum/2,
+	for/3
+]).
+
 sum(L) ->
-	sum(l, 0).
+	sum(L, 0).
 
 sum([], N) ->
 	N;
 sum([H|T], N) ->
 	sum(T, H + N).
+
+for(Max, Max, F) ->
+	[F(Max)];
+for(I, Max, F) ->
+	[F(I) | for(I + 1, Max, F)].
